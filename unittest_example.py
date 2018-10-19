bc65443f414f53091ef2cb8c80efac63af57914a
Wade003 <1024145057@qq.com>
2018-10-19 11:50:48 +0100

    Update unittest_example.py

--- unittest_example.py
+++ unittest_example.py
@@ -4,6 +4,6 @@ import foo
 class TestSolveQuadratic(unittest.TestCase):
     def test_solve_quadratic(self):
     
-        self.assertEqual(foo.solve_quadratic(0,0,-1), (-1.0,1.0))
+        self.assertEqual(foo.solve_quadratic(1,0,-1), (-1.0,1.0))
         self.assertEqual(foo.solve_quadratic(1,0,0), 0.0 )
         self.assertEqual(foo.solve_quadratic(1,0,1), None)
